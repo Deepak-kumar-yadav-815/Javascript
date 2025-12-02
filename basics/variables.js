8772adb62b2b84d551e5fc44ff05bca39e47e2c3
Deepak kumar yadav <deepakkyadav815@gmail.com>
2025-12-03 00:35:48 +0530

    variables of javascript

---
+++ basics/variables.js
@@ -0,0 +1,26 @@
+//let var const
+const accountId = 455
+let accountName = "Deepak"
+var accountPassword = "CodeExplorerDKY"
+accountCity = "ranchi"
+// accountId = 871 {
+//     not allowed in javascript we cannot change the variable name in javascript
+// }
+accountName = "Ashish"
+accountPassword = "hi@2005"
+console.log(accountId);
+console.table([accountName,accountPassword,accountCity,accountId])
+
+
+
+/*
+ never use var in javascript beacuse of its scoping
+ problem
+ @always use let or const
+*/
+
+
+// in javascript if you put varaible undeclred its value will un undifened
+let deepak
+console.log(deepak);
+
