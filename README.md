29f5d1d728d8d2b9210f8c7e0d6edda7e5213fb5
Deepak kumar yadav <deepakkyadav815@gmail.com>
2025-12-03 07:01:21 +0000

    readme file added

---
+++ README.md
@@ -0,0 +1,1 @@
+[javascript tutorial or ducumentaion from mdn](https://developer.mozilla.org/en-US/docs/Web/JavaScript)https://developer.mozilla.org/en-US/docs/Web/JavaScript
---
+++ basics/operations.js
@@ -0,0 +1,32 @@
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// *************operations***********\\
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)//power
+// console.log(2/3)
+// console.log(2%2) //remainder
+
+// let str = "Deepak"
+// let str2 = " kumar"
+// let str3 = str+ str2
+// console.log(str3);
+
+console.log("1"+2)
+console.log(1+"2")
+console.log(2+2+"2")
+console.log("2"+2+2)
+/**
+ thier outputs
+12
+12
+42
+222
+ */
+console.log(+true);
+console.log(+"");
