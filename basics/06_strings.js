7536cc1c36d680665cb09dfddaeb4a17a04e1e3d
Deepak kumar yadav <deepakkyadav815@gmail.com>
2025-12-03 14:48:20 +0530

    string,js

---
+++ basics/06_strings.js
@@ -0,0 +1,45 @@
+// const name = "Deepak kumar yadav"
+console.log(name);
+let repocount = 54
+console.log(repocount);
+console.log(`hello my name is ${name} and my repocount is ${repocount}`);
+
+
+//decaletion 
+// const gameName = new String('Temple_run')
+
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
