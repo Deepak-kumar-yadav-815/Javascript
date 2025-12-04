4576f275bd3de083de162628fd9c1e87d645c5cb
Deepak kumar yadav <deepakkyadav815@gmail.com>
2025-12-04 16:18:33 +0530

    doing directory sturcture

---
+++ 01_basics/data_types.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) // we are using nodjes not in browswer
+
+
+let name = "deepak" // string
+let age  = 22 // number
+let isPassed = true //boolean
+
+/*
+ data types:-
+ 1 - string
+ 2 - number
+ 3 - boolean
+ 4 - bigint
+ 5 - null => standalone value
+ 6 - undifined => 
+ 7 - symbol=> unique
+
+ //above are of primitive type
+ //below are composite data type
+ 8 object
+*/
+
+console.log(typeof undefined);//undifined
+console.log(typeof null);// object importenat in interview
+
