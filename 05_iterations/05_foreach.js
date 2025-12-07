b711638147949d82954b24739231be5e6e0efd59
Deepak kumar yadav <deepakkyadav815@gmail.com>
2025-12-07 14:26:26 +0530

    foreach

--- 05_iterations/05_foreach.js
+++ 05_iterations/05_foreach.js
@@ -16,3 +16,16 @@ let myArr = ["js","cpp","py","c","ruby"]
 myArr.forEach( (item,index,arr) => {
     console.log(item,index,arr);
  })
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter( (num) => num>4)
+const newNums = myNums.filter( (num) => {
+    return num>4
+})
+
+/**
+ * jb bhi () yeah likho do not write return statement
+ * {} => yeah likho toh write return statement
+ */
+console.log(newNums);
