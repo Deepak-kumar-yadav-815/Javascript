4c887da051bc0581286e1a86cc48ba04fddc8eab
Deepak kumar yadav <deepakkyadav815@gmail.com>
2025-12-07 10:58:18 +0530

    behinf exexcution

---
+++ 03_basics/05_javascript_execution_context.js
@@ -0,0 +1,19 @@
+/**
+ * javascript is single theraded language
+ * 
+ * 
+ */
+
+/**
+ * global exection context
+ * function exection context
+ * eval execution context
+ * {} -> memory creation phase
+ *    -> exection phase
+ */
+// see the attested image
+
+/**
+ * call stack():-
+ * call stack = ["global execution",""]
+ */
